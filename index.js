8e61a7fc4d3bcdcdd58ddb739f55a68cac2bcd31
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-29 07:00:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ function sayHiToGrandma(string){
   if (string === lowercase){
     return 'I can\'t hear you!'
   }
-  elseif (string === uppercase){
+  else if (string === uppercase){
     return 'YES INDEED!'
+  }
+  else if (string === 'I love you, Grandma') {
+return 'I love you, too.'    
   }
 }
